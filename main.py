934026a3034b7f86c206866219711fcff426b096
arseniiboiko2010 <114818838+arseniiboiko2010@users.noreply.github.com>
2023-04-11 16:03:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,41 +9,23 @@ class MediaPlayer(QWidget):
 
     def __init__(self, video_file):
         super().__init__()
-
         self.current_volume = 50  # set initial volume to 50%
         self.media_player = vlc.MediaPlayer()
-        
         current_volume = self.media_player.audio_get_volume()
         new_volume = min(current_volume + 10, 100)
         self.media_player.audio_set_volume(new_volume)
-
         self.setGeometry(100, 100, 600, 400)
-
-        # Встановлення заголовку вікна
         self.setWindowTitle("Медіаплеєр")
-
-        # Створення графічного інтерфейсу
         self.layout = QVBoxLayout()
-
-        # Створення медіаплеєра
         self.media_player = vlc.MediaPlayer()
-
-        # Відтворення відео
         self.media = vlc.Media(video_file)
         self.media_player.set_media(self.media)
-
-        # Встановлення видимості відео
         self.frame = QFrame(self)
         self.frame.setFrameShape(QFrame.Box)
         self.frame.setLineWidth(2)
         self.frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.media_player.set_hwnd(self.frame.winId())
-
-        # Відтворення відео
         self.media_player.play()
-
-
-        # Створення кнопок для управління медіаплеєром
         self.time_up_button = QPushButton(">>", self)
         self.time_down_button = QPushButton("<<", self)
         self.volume_up_button = QPushButton("+", self)
@@ -51,14 +33,10 @@ class MediaPlayer(QWidget):
         self.open_button = QPushButton("Open", self)
         self.play_button = QPushButton("Play", self)
         self.pause_button = QPushButton("Pause", self)
-
-        # Встановлення розміру кнопок
         self.volume_up_button.setFixedSize(20, 20)
         self.volume_down_button.setFixedSize(20, 20)
         self.time_up_button.setFixedSize(20, 20)
         self.time_down_button.setFixedSize(20, 20)
-
-        # Додавання функціоналу до кнопок
         self.time_up_button.clicked.connect(self.time_up)
         self.time_down_button.clicked.connect(self.time_down)
         self.volume_up_button.clicked.connect(self.volume_up)
@@ -66,9 +44,6 @@ class MediaPlayer(QWidget):
         self.open_button.clicked.connect(self.open_media)
         self.play_button.clicked.connect(self.play_media)
         self.pause_button.clicked.connect(self.pause_media)
-
-        # Створення графічного інтерфейсу
-        
         hbox2 = QHBoxLayout()
         hbox2.addWidget(self.time_down_button)
         hbox2.addWidget(self.time_up_button)
@@ -91,11 +66,8 @@ class MediaPlayer(QWidget):
             self.media = vlc.Media(file_name)
             self.media_player.set_media(self.media)
             self.media_player.play()
-
     def play_media(self):
         self.media_player.set_time(0)
-
-
     def pause_media(self):
         if self.media_player.is_playing():
             self.media_player.pause()
@@ -105,20 +77,14 @@ class MediaPlayer(QWidget):
         new_volume = min(self.current_volume + 5, 100)
         self.media_player.audio_set_volume(new_volume)
         self.current_volume = new_volume
-
     def volume_down(self):
         new_volume = max(self.current_volume - 5, 0)
         self.media_player.audio_set_volume(new_volume)
         self.current_volume = new_volume
     def time_up(self):
         self.media_player.set_time(self.media_player.get_time() + 10000)
-
-
     def time_down(self):
         self.media_player.set_time(self.media_player.get_time() - 5000)
-
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('video_file', help='The path to the video file.')
